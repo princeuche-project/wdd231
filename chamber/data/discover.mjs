beb9f74b1ce77018c0ec79410fad6c340899f5d7
uchenna33 <uowuamanamgodstime@byupathway.edu>
2026-08-03 00:20:22 +0400

    added

--- chamber/data/discover.mjs
+++ chamber/data/discover.mjs
@@ -1,56 +1,56 @@
 export const places = [{
         id: 1,
         name: "Oguta Lake",
-        image: "images/oguta_lake.jpg",
+        image: "images/Oguta_lake.webp",
         address: "Oguta, Imo State",
         description: "One of Nigeria's most beautiful natural lakes and a popular tourist attraction."
     },
     {
         id: 2,
         name: "Mbari Cultural Center",
-        image: "images/mbari_cultural_center.jpg",
+        image: "images/mbari_cultural_center.webp",
         address: "Owerri, Imo State",
         description: "A center preserving Igbo art, history and culture."
     },
     {
         id: 3,
         name: "Dan Anyiam Stadium",
-        image: "images/stadium.jpg",
+        image: "images/stadium.webp",
         address: "Port Harcourt Road, Owerri",
         description: "The major sporting stadium in Imo State."
     },
     {
         id: 4,
         name: "Maria Assumpta Cathedral",
-        image: "images/assumptal_carthdral.jpg",
+        image: "images/assumptal_carthdral.webp",
         address: "Control Post, Owerri",
         description: "One of the largest Catholic churches in southeastern Nigeria."
     },
     {
         id: 5,
         name: "Owerri Mall",
-        image: "images/owerri_mall.jpg",
+        image: "images/owerri_mall.webp",
         address: "Egbu Road",
         description: "Modern shopping center with restaurants and entertainment."
     },
     {
         id: 6,
         name: "Nekede Zoo",
-        image: "images/nekede_zoo.jpg",
+        image: "images/nekede_zoo.webp",
         address: "Nekede, Owerri",
         description: "Wildlife conservation and recreational destination."
     },
     {
         id: 7,
         name: "Heroes Square",
-        image: "images/hero_square.jpg",
+        image: "images/hero_square.webp",
         address: "New Owerri",
         description: "A public square used for celebrations and national events."
     },
     {
         id: 8,
         name: "Imo Golf Club",
-        image: "images/imo_gulf.jpg",
+        image: "images/imo_gulf.webp",
         address: "Owerri",
         description: "Beautiful golf course with relaxing scenery."
     }
